d37dee509922f649c6005d088ec4d48f0116ccd4
dileepadari <adaridileep@gmail.com>
2023-05-08 21:04:07 +0530

    Revert "delhi changes 8:16 last day"
    This reverts commit 9503dcbc746c7a06ea5d5a3557668728adddd731.

--- src/app.py
+++ src/app.py
@@ -13,6 +13,7 @@ all_artists = results.fetchall()
 app = Flask(__name__)
 app.secret_key = 'DelhiisDelhi'
 
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     connection = sqlite3.connect("Music_Mania.db")
@@ -54,13 +55,10 @@ def spotlight_page():
     connection = sqlite3.connect("Music_Mania.db")
     curser = connection.cursor()
 
-    query = "SELECT * FROM Artists where uniqid = 12"
+    query = "SELECT * FROM Artists where index = 12"
     results = curser.execute(query)
     all_artists = results.fetchall()
-    query = 'SELECT * FROM Albums WHERE "index" = 12'
-    album_list = curser.execute(query)
-    albums = album_list.fetchall()
-    return render_template('spotlight.html', artist=all_artists, albums=albums)
+    return render_template('spotlight.html', artist=all_artists)
 
 @app.route('/artist/<artist_name>')
 def artist_page(artist_name):
